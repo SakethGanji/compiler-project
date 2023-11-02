e40d5567857fb9716fb3413fdc6d878bf074d91f
Saketh Ganji <bsg200001@utdallas.edu>
2023-11-02 10:30:28 -0500

    { Fielddecls Stmts } Optionalsemi

--- BlockStmt.java
+++ BlockStmt.java
@@ -1,14 +1,16 @@
 public class BlockStmt extends Stmt {
+    private Fielddecls fielddecls;
     private Stmts stmts;
     private boolean optionalsemi;
 
-    public BlockStmt(Stmts stmts, boolean optionalsemi) {
+    public BlockStmt(Fielddecls fielddecls, Stmts stmts, boolean optionalsemi) {
+        this.fielddecls = fielddecls;
         this.stmts = stmts;
         this.optionalsemi = optionalsemi;
     }
 
     public String toString(int t) {
         String semi = optionalsemi ? ";" : "";
-        return getTabs(t) + "{\n" + stmts.toString(t + 1) + getTabs(t) + "}" + semi + "\n";
+        return getTabs(t) + "{\n" + fielddecls.toString(t + 1) + stmts.toString(t + 1) + getTabs(t) + "}" + semi + "\n";
     }
 }
